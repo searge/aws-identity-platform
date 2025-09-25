e72e0f22cee4d992793c30efc8d264ab021532c3
Searge <searge@duck.com>
2025-09-25 17:58:48 +0300

    fix(ic): Add versions and extent variables

--- modules/identity_center/variables.tf
+++ modules/identity_center/variables.tf
@@ -4,7 +4,42 @@ variable "accounts" {
   default     = {}
 }
 
-variable "users" {}
-variable "groups" {}
-variable "permission_sets" {}
-variable "account_assignments" {}
+variable "users" {
+  description = "A map of users to create in IAM Identity Center."
+  type = map(object({
+    given_name  = string
+    family_name = string
+    email       = string
+  }))
+  default = {}
+}
+
+variable "groups" {
+  description = "A map of groups to create in IAM Identity Center."
+  type = map(object({
+    description = string
+    members     = list(string)
+  }))
+  default = {}
+}
+
+variable "permission_sets" {
+  description = "A map of permission sets to create in IAM Identity Center."
+  type = map(object({
+    description         = string
+    session_duration    = optional(string, "PT1H")
+    managed_policy_arns = optional(list(string), [])
+  }))
+  default = {}
+}
+
+variable "account_assignments" {
+  description = "A list of assignments to link groups/users to permission sets on accounts."
+  type = list(object({
+    principal_name = string
+    principal_type = string
+    account_name   = string
+    permission_set = string
+  }))
+  default = []
+}
