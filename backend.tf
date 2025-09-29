a86f9d51f9ee5435e41b2b32fcc8f46951cafe7a
Searge <searge@duck.com>
2025-09-29 20:52:47 +0300

    :truck: Remove `env` files to use one root env

--- backend.tf
+++ backend.tf
@@ -1,8 +1,5 @@
 terraform {
   backend "local" {
-    # Configuration via backend.tfvars files
-    # Example usage:
-    # terraform init -backend-config=env/dev/backend.tfvars
-    # terraform init -backend-config=env/prod/backend.tfvars
+    path = "terraform.tfstate"
   }
 }
